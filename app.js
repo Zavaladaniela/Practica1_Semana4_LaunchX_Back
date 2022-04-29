f046e552b3e8cb640d4f24fb3180a7dd9441fd4d
Zavaladaniela <daniela.zavala107@gmail.com>
2022-04-28 22:34:01 -0500

    Refectorice para el filtrado por mision, probe con un index y porteriormente desarrollare las pruebas necesarias

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 
 
-// Part 2: Get the quantity of explorers names in node
-const explorersInNode = explorers.filter((explorer) => explorer.mission == "node");
-//console.log(explorersInNode.length)
+
 
 // Part4: Get the explorer's usernames in Node
 const explorersInNodeToGetUsernames = explorers.filter((explorer) => explorer.mission == "node");
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const reader = require ("./../refactoring/lib/utils/reader")
+const explorerService = require ('./../refactoring/lib/services/explorerService')
 
 const explorers = reader.readJsonFile("explorers.json")
-
 console.log (explorers)
+
+console.log ("Desarrollar Test2: Filtrar por mision node")
+console.log (explorerService.filterByMission (explorers, "node"));
+
+// Aplicación del ExplorerService sobre la lista de explorers
+// ExplorerService.filterByMission(explorers, "node");
+// ExplorerService.getAmountOfExplorersByMission(explorers, "node");
+// ExplorerService.getExplorersUsernamesByMission(explorers, "node");
