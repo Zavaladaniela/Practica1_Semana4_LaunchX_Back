fb76a86bf95011308189e9fbab153f7bdb012a20
Zavaladaniela <daniela.zavala107@gmail.com>
2022-04-28 23:36:59 -0500

    Desarrolle la prueba unitaria para la lectura del archivo, tiene una falla

--- test/services/reader.test.js
+++ test/services/reader.test.js
@@ -1,5 +1,8 @@
-const reader = require('./../../lib/utils/reader')
+const Reader = require('./../../lib/utils/reader')
 
-describe ("Test 1: Prueba para la lectura del archivo", () =>{
-    
+describe ("Prueba de jest", () =>{
+    test ("Test 1: Prueba para la lectura del archivo ", () =>{
+        const reader = new Reader('explorers.json')
+        expect(reader.readJsonFile).toBe ('explorers.json')
+    });
 })
